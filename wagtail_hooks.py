72dc0247724e14e85fb11731abaea1df279647dd
danlerche <dan.lerch@gmail.com>
2023-11-10 15:57:43 -0800

    added registration project files for dev branch

--- wagtail_hooks.py
+++ wagtail_hooks.py
@@ -1,11 +1,17 @@
 from wagtail.snippets.models import register_snippet
-from wagtail.snippets.views.snippets import SnippetViewSet, SnippetViewSetGroup
+from wagtail.snippets.views.snippets import SnippetViewSet, SnippetViewSetGroup, DeleteView
+from wagtail.admin.panels import FieldPanel, TabbedInterface, ObjectList
 from wagtail.admin.ui.tables import UpdatedAtColumn
-from .models import (EventCategory, Event,EventAudience, EventAge, EventAge, FullCalendarLink)
+from .models import (EventCategory, Event, EventAudience, EventAge, EventAge, FullCalendarLink, RegistrationUserFormBuilder, Registration, RegistrationFormPage)
 from django.templatetags.static import static
 from django.utils.html import format_html
-from wagtail import hooks
 from wagtail.admin.filters import WagtailFilterSet
+import json, csv
+from django.http import HttpResponse
+from wagtail import hooks
+from wagtail.snippets.bulk_actions.snippet_bulk_action import SnippetBulkAction
+from wagtail.admin.menu import MenuItem
+from wagtail.admin.views.bulk_action import BulkAction
 
 class EventCategoryAdmin(SnippetViewSet):
     model = EventCategory
@@ -46,10 +52,125 @@ class LinkToCalendarAdmin(SnippetViewSet):
     icon = 'link'
     base_url_path = "calendar_link"
 
+class DeleteUserInfoView(DeleteView):
+    #deletes the user info when a regration is also deleted
+    def get_success_message(self):
+        cancel_user = RegistrationUserFormBuilder.objects.get(id=self.object.user_info.id)
+        cancel_user.delete()
+        msg = 'The following user info has been deleted:  ' + str(cancel_user)
+        return msg
+
+class FilterByEvent(WagtailFilterSet):
+    class Meta:
+        model = Registration
+        fields = ['event_name']
+
+class RegistrationAdmin(SnippetViewSet):
+    model = Registration
+    menu_label = 'Registrations' 
+    icon = 'doc-full'
+    base_url_path = 'registration'
+    list_display = ('name','email', 'event_name', 'registration_date' ,'wait_listed')
+    index_template_name = 'library_programs/registration/index.html'
+    filterset_class = FilterByEvent
+    delete_view_class = DeleteUserInfoView
+    edit_template_name = 'library_programs/registration/admin_snippet/edit.html'
+
+    edit_handler = TabbedInterface([
+        ObjectList([FieldPanel("registration_date", read_only=True), FieldPanel("user_info", read_only=True), FieldPanel("wait_list")], 
+            heading="Registrant"),
+    ])
+
+#deletes the registration form entry when the registration is deleted
+    @hooks.register('after_delete_snippet')
+    def after_snippet_delete(request, objects):
+        for obj in objects: 
+            user_id = obj.user_info.id
+            user_info = JoinusUserFormBuilder.objects.get(id=user_id)
+            user_info.delete()
+        #return HttpResponse(f"{user_info} has been deleted", content_type="text/plain")
+        return request
+
+@hooks.register('register_bulk_action')
+class ExportCSV(BulkAction):
+    display_name = "Export CSV"
+    aria_label = "Export CSV"
+    action_type = "export_csv"
+    template_name = "library_programs/registration/admin_snippet/export_csv.html"
+    models = [Registration]
+    
+    @hooks.register("before_bulk_action")
+    def hook_function(request, action_type, objects, action_class_instance, **kwargs):
+        if action_type == 'export_csv':
+            fields = []
+            response = HttpResponse(content_type='text/csv')
+            response['Content-Disposition'] = 'attachment; filename="export_registration_info.csv"'
+            writer = csv.writer(response)
+
+            for obj in objects:
+                #the user_info labels are extracted from json in a single field
+                user_fields = list(obj.user_info.form_data.keys())
+                #convert the obj class to dictionary and extract the keys
+                obj_dic_keys = vars(obj).keys()
+                #convert to a list
+                obj_keys_list = list(obj_dic_keys)
+            #remove uneeded labels
+            obj_keys_list.remove('user_info_id')
+            obj_keys_list.remove('_state')
+
+            #clean up names
+            static_fields = []
+            for okl in obj_keys_list:
+                okl_replace = okl.replace("event_name_id", "event_name")
+                static_fields.append(okl_replace)
+
+            fields = static_fields + user_fields
+
+            field_labels = []
+            for flds in fields:
+                field_labels.append(flds.replace("_", " ").title())
+            writer.writerow(field_labels)
+
+            user_info_values = []
+            static_values = []
+            for obj in objects:
+                user_info_values.append(obj.user_info.form_data.values())
+                if obj.wait_list == True:
+                    wait_list = 'Yes'
+                elif obj.wait_list == False:
+                    wait_list = 'No'
+                static_values.append(list((obj.id, str(obj.event_name), obj.registration_date.strftime("%Y-%m-%d %H:%M"), wait_list)))
+
+            user_values = [list(val) for val in user_info_values]
+
+            #clean up boolean field output
+            for user_info in user_values:
+                if ['No'] in user_info:
+                    user_info[user_info.index(['No'])] = 'No'
+                if ['Yes'] in user_info:
+                    user_info[user_info.index(['Yes'])] = 'Yes'
+
+            registration_values = []
+
+            for static, user in zip(static_values, user_values):
+                registration_values.append(static + user)
+
+            for rv in registration_values:
+                writer.writerow(rv)
+            #uncomment to test output without exporting a CSV
+            #return HttpResponse(f"{registration_values}", content_type="text/plain")
+            return response
+
+class RegistrationFormAdmin(SnippetViewSet):
+    model = RegistrationFormPage
+    menu_label = 'Registration Form'
+    icon = 'form'
+    base_url_path = "registration-form"
+
 class EventAdminGroup(SnippetViewSetGroup):
     menu_label = 'Programs & Events'
     menu_icon = 'date'
     menu_order = 200
-    items = (EventAdmin, EventCategoryAdmin, EventAudienceAdmin, EventAgeAdmin, LinkToCalendarAdmin)
+    items = (EventAdmin, EventCategoryAdmin, EventAudienceAdmin, EventAgeAdmin, LinkToCalendarAdmin, RegistrationFormAdmin, RegistrationAdmin)
 
 register_snippet(EventAdminGroup)
